2795a4bb0b9647709aefdf85c58f60996a20cb59
Chris Rossi <chris@archimedeanco.com>
2012-02-13 16:51:28 -0500

    Display sphinx page properly.

--- src/scribble/scribble/views.py
+++ src/scribble/scribble/views.py
@@ -5,4 +5,6 @@ from scribble.models import SphinxDocument
 
 @view_config(context=SphinxDocument, renderer='sphinxdoc.pt')
 def view_sphinx_document(context, request):
+    import pprint
+    pprint.pprint(context.__dict__)
     return {}
